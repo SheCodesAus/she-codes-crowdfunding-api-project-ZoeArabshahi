848b3e98d6eaa2af56521f1d578497ab1fc31bcd
Zoe Arabshahi <z.arabshahi59@gmail.com>
2022-06-25 12:27:59 +1000

    put a comment

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,26 @@
+from email.mime import image
+from turtle import title
+from unittest.util import _MAX_LENGTH
+from xml.etree.ElementTree import Comment
 from django.db import models
 
-# Create your models here.
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+        )
+    supporter = models.CharField(max_length=200)
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description=models.TextField()
+    goal=models.IntegerField()
+    image=models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+    owner = models.CharField(max_length=200)
+
